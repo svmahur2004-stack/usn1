98ae28a433195f559ab9e4d4a8ac2e5acd03530b
svmahur2004-stack <svmahur2004@gmail.com>
2025-09-30 10:34:25 +0530

    master branch

---
+++ usn1.py
@@ -0,0 +1,10 @@
+# Taking input from the user
+name = input("Enter your name: ")
+usn = input("Enter your USN: ")
+course = input("Enter your course: ")
+
+# Printing the details
+print("\n--- Student Details ---")
+print("Name   :", name)
+print("USN    :", usn)
+print("Course :", course)
