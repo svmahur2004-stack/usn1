b911f3432eaed62bf525aa2251064f51e942711b
svmahur2004-stack <svmahur2004@gmail.com>
2025-09-30 10:47:28 +0530

    master branch

--- usn1.py
+++ usn1.py
@@ -2,9 +2,10 @@
 name = input("Enter your name: ")
 usn = input("Enter your USN: ")
 course = input("Enter your course: ")
-
+college = input("Enter your college:")
 # Printing the details
 print("\n--- Student Details ---")
 print("Name   :", name)
 print("USN    :", usn)
 print("Course :", course)
+print("College:", college) 
